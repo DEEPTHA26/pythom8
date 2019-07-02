5832531ca5b1b70e192d6aa002f2b2954d19b901
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-07-02 11:43:55 +0530

    armss.py

--- armss.py
+++ armss.py
@@ -1,11 +1,11 @@
-d=int(input())
-e=0
-f=d
-while f>0:
-  num=f%10
-  e+=num**3
-  f//10
-if d==e:
+n=int(input())
+h=0
+a=h
+while a>0:
+  num=a%10
+  h+=num**3
+  a//10
+if n==h:
   print("yes")
 else:
   print("No")
